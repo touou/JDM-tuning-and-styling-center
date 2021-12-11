c6bb39ab80729755790d194ee706921a4252cb9a
touou <72223183+touou@users.noreply.github.com>
2021-12-11 03:40:28 +0300

    new journey is about to begin

---
+++ JDMTuneV2/Data/DataRepository.cs
@@ -0,0 +1,26 @@
+﻿using JDMTuneV2.Models;
+using LinqToDB;
+using LinqToDB.Configuration;
+using LinqToDB.Data;
+
+namespace JDMTuneV2.Data
+{
+    public class DataRepository
+    {
+        private DataConnection Connection { get; }
+
+        public DataRepository(string connectionString)
+        {
+            var optionsBuilder = new LinqToDbConnectionOptionsBuilder();
+            optionsBuilder.UsePostgreSQL(connectionString);
+            Connection = new DataConnection(optionsBuilder.Build());
+        }
+
+        public ITable<User> Users => Connection.GetTable<User>();
+
+        public void AccountCreate(User acc)
+        {
+            Connection.Insert(acc);
+        }
+    }
+}
---
+++ JDMTuneV2/Helpers/JwtService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JDMTuneV2.Helpers
+{
+    public class JwtService
+    {
+        private string securityKey = "ya ebal v rot jwt tokeni";
+
+        public string GenerateToken(Guid id)
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+            var securityToken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+
+        public JwtSecurityToken Verify(string jwt)
+        {
+            var tokenHandler =new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(securityKey);
+            tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+            }, out SecurityToken validatedToken);
+
+            return (JwtSecurityToken) validatedToken;
+        }
+    }
+}
